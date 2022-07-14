54b96ec1212a054953464559cc4c933ee948b8da
Boldinov <pashaboldinov@gmail.com>
2022-07-14 20:19:05 +0500

    1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -0,0 +1,16 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    User findByName(String name);
+    void addUser(User user);
+    void deleteUser(Long id);
+    void updateUser(User user);
+    List<User> getAllUsers();
+    List<Role> getAllRoles();
+    User getUser(Long id);
+}
