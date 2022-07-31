cc341c357f1c8958bf424a35e9cd8f7e0639b317
Paul <pashaboldinov@gmail.com>
2022-07-31 11:15:12 +0600

    worked final

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -31,7 +31,7 @@ public class User implements UserDetails {
     private String password;
 
     @Column
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name="users_roles",
             joinColumns=@JoinColumn(name="user_id"),
